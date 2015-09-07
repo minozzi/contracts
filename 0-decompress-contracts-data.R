46a494222c2632148ad59cc35531702ca2bc3162
Austin Knuppe <AJKnuppe@users.noreply.github.com>
2015-09-07 11:35:29 -0400

    Create 0-decompress-contracts-data.R

---
+++ 0-decompress-contracts-data.R
@@ -0,0 +1,20 @@
+## =============================================================================
+## Title:  Decompress raw contracts data
+## Author: Austin Knuppe (knuppe.2@osu.edu)
+## Project: Government Contracts Data
+## Last Updated: 7 Sept 2015
+## Data Source: USASpending.gov
+## =============================================================================
+
+## Script Purpose: decompress each monthly time slice of the contracts data and 
+## save it
+
+# Set working directory
+setwd("~/Data/contracts")
+
+# Read through all files, load each one, then save 
+files <- list.files()
+
+for(fn in files){
+        load(fn)
+        save(List = "dat",  file = fn)
