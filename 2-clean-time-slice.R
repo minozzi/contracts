f96dade5522fab31868c7ddc748403c3860d4106
Austin Knuppe <AJKnuppe@users.noreply.github.com>
2015-09-23 09:15:43 -0400

    updates to dataset cleaning

--- 2-clean-time-slice.R
+++ 2-clean-time-slice.R
@@ -2,15 +2,17 @@
 ## Title:  Clean an annual time slice of the contracts data 
 ## Author: Austin Knuppe (knuppe.2@osu.edu)
 ## Project: Government Contracts Data 
-## Last Updated: 14 Sept 2015
+## Last Updated: 23 September 2015
 ## Data Source: USASpending.gov
+## Operating System: Mac OS 10.10.5
+## Statistical Software: RStudio Version 0.99.473
 ## =============================================================================
 
-## Script Purpose: Subset and clean a single annual slice of the contracts data
+## Script Purpose: Subsect and clean a single annual slice of the contracts data
 
 ##------
 # set wd
-# setwd("~/Data/contracts")
+# setwd("~/Data/")
 
 ##----------------------------------------
 # erase previous objects in system memory
@@ -18,113 +20,130 @@ rm(list=ls())
 
 #---------------
 # load libraries
-library(plyr)
+library(data.table)
 library(tidyr)
 
 ##-----------
 # set options
 options(stringsAsFactors = FALSE)
+# default.stringsAsFactors() # now FALSE
 
 ##---------
 # load data
-load("~/Data/contracts/contracts-1999.rdata") # change for each yr
+load("~/Data/contracts/annual-contracts/contracts-2014.RData")
+dat <- as.data.table(dat)
+setkey(dat)
 
-##--------------------- 
-# add rownumbers to dat
-dat$rownumber = 1:nrow(dat)
+##------------------------- 
+# remove useless covariates
+dat[ , majorprogramcode := NULL]
+dat[ , isforeigngovernment := NULL]
+dat[ , isforeignownedandlocated := NULL]
+dat[ , informationtechnologycommercialitemcategory := NULL]
+dat[ , fundedbyforeignentity := NULL]        
+        
+##--------------------------------------
+# turn negative dollars obligated into 0
+neg_dol <- dat[dat$dollarsobligated < 0, ]  
 
-#---------------------------------------------------------
-# split strings for variables with unique id : description
+neg_dol$dollarsobligated <- 0
 
+dat <- rbind(neg_dol, dat)
+
+table(duplicated(dat$unique_transaction_id))
+
+dat <- dat[!duplicated(unique_transaction_id)]
+
+rm(neg_dol)
+
+##---------------------- 
+# subset by major agency
+dat <- separate(dat, maj_agency_cat, 
+                c("maj_agency_cat", "maj_agency"), 
+                ": ", 
+                extra = "merge")
+
+# DOD, State, and DHS
+dat <- dat[dat$maj_agency_cat == "9700" |
+                   dat$maj_agency_cat == "7000" |
+                   dat$maj_agency_cat == "1900", ]
+
+#---------------------------------------------------------------
+# split strings for other variables with unique id : description
 dat <- separate(dat, signeddate, 
                 c("signed_year", "signed_month", "signed_day"), 
                 "-", 
                 extra = "merge")
 
-dat <- separate(dat, maj_agency_cat, 
-                     c("maj_agency_cat", "maj_agency"), 
-                     ": ", 
-                     extra = "merge")
-
 dat <- separate(dat, maj_fund_agency_cat, 
-                     c("maj_fund_agency_cat", 
-                     "maj_fund_agency"), 
-                     ": ", 
-                     extra = "merge")
+                c("maj_fund_agency_cat", 
+                  "maj_fund_agency"), 
+                ": ", 
+                extra = "merge")
 
 dat <- separate(dat, agencyid, 
-                     c("agency_id", "agency"), 
-                     ": ", 
-                     extra = "merge")
+                c("agency_id", "agency"), 
+                ": ", 
+                extra = "merge")
 
 dat <- separate(dat, mod_agency, 
-                     c("mod_agency_id", "mod_agency"), 
-                     ": ", 
-                     extra = "merge")
+                c("mod_agency_id", "mod_agency"), 
+                ": ", 
+                extra = "merge")
 
 dat <- separate(dat, contractingofficeagencyid, 
-                     c("contracting_agency_id", "contracting_agency"), 
-                     ": ", 
-                     extra = "merge")
+                c("contracting_agency_id", "contracting_agency"), 
+                ": ", 
+                extra = "merge")
 
 dat <- separate(dat, fundingrequestingagencyid, 
-                     c("funding_request_agency_id", "funding_request_agency"), 
-                     ": ", 
-                     extra = "merge")
+                c("funding_request_agency_id", "funding_request_agency"), 
+                ": ", 
+                extra = "merge")
 
 dat <- separate(dat, contractingofficeid, 
-                     c("contracting_office_id", "contracting_office"), 
-                     ": ", 
-                     extra = "merge")
+                c("contracting_office_id", "contracting_office"), 
+                ": ", 
+                extra = "merge")
 
 dat <- separate(dat, fundingrequestingofficeid, 
-                     c("funding_request_office_id", "funding_request_office"), 
-                     ": ", 
-                     extra = "merge")
+                c("funding_request_office_id", "funding_request_office"), 
+                ": ", 
+                extra = "merge")
 
 dat <- separate(dat, systemequipmentcode, 
-                          c("sec", "sec_descrip"),  
-                          ": ", 
-                          extra = "merge")
+                c("sec", "sec_descrip"),  
+                ": ", 
+                extra = "merge")
 
 dat <- separate(dat, principalnaicscode, 
-                          c("naics", "naics_descrip"),  
-                          ": ",
-                          extra = "merge")
+                c("naics", "naics_descrip"),  
+                ": ",
+                extra = "merge")
 
 dat <- separate(dat, productorservicecode, 
-                          c("psc", "psc_descrip"),  
-                          ": ",
-                          extra = "merge")
+                c("psc", "psc_descrip"),  
+                ": ",
+                extra = "merge")
 
 dat <- separate(dat, claimantprogramcode, 
-                          c("cpc", "cpc_descrip"),  
-                          ": ",
-                          extra = "merge")
+                c("cpc", "cpc_descrip"),  
+                ": ",
+                extra = "merge")
 
-dat <- separate(dat, informationtechnologycommercialitemcategory, 
-                          c("tech_cat", "tech_descrip"),  
-                          ": ",
-                          extra = "merge")
+dat <- separate(dat, placeofperformancecountrycode, 
+                c("pop_country_code", "pop_country"),  
+                ": ",
+                extra = "merge")
 
-##-----------------------
-# Make into R Date format
-dat$effectivedate          <- as.Date(dat$effectivedate, "%m/%d/%Y")
-dat$last_modified_date     <- as.Date(dat$last_modified_date, "%m/%d/%Y")
-dat$signeddate             <- as.Date(dat$signeddate, "%m/%d/%Y")
-dat$currentcompletiondate  <- as.Date(dat$currentcompletiondate, "%m/%d/%Y")
-dat$ultimatecompletiondate <- as.Date(dat$ultimatecompletiondate, "%m/%d/%Y")
-dat$lastdatetoorder        <- as.Date(dat$lastdatetoorder, "%m/%d/%Y")
+setnames(dat, "vendorcountrycode", "vendor_country")
 
-##----------------------------------------------------------------------- 
-# let's only save those contracts within State, DOD, or Homeland Security
-dat <- dat[dat$maj_agency_cat == "9700" |
-                   dat$maj_agency_cat == "7000" |
-                   dat$maj_agency_cat == "1900", ]
+##-----------------------------------
+# save the time slice of cleaned data
+# change for each annual time slice
+save(dat, file = "~/Data/contracts/clean-contracts/clean-2014.rdata") 
 
-##---------------------------------------------
-# save the partially-cleaned and subsetted data
-save(dat, file = "~/Data/contracts/clean-1999.rdata") # change for each year
+rm(dat)
 
 ###############################################################################
 ## EOF
