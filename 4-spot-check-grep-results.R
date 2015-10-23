cb602c4250f237de5c2a4243963f66554e41f8d8
Austin Knuppe <AJKnuppe@users.noreply.github.com>
2015-10-23 11:35:39 -0400

    Update 4-spot-check-grep-results.R

--- 4-spot-check-grep-results.R
+++ 4-spot-check-grep-results.R
@@ -1,1 +1,11 @@
+## =============================================================================
+## Title:  Spot check and clean MDAP
+## Author: Austin Knuppe (knuppe.2@osu.edu)
+## Project: Contracts Data
+## Last Updated: 23 Oct. 2015
+## Specs: Mac OS 10.11, RStudio v. 0.99.486, R v. 3.2.2
+## =============================================================================
+
+## Script Purpose: remove false positives from each MDAP
+## Packages Required: data.table
 
