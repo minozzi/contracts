9e6a6047ffe52529e348f82c050ab004594a0a17
Austin Knuppe <AJKnuppe@users.noreply.github.com>
2015-10-31 16:41:35 -0400

    Update 3b-search-cols-to-grep.R

--- 3b-search-cols-to-grep.R
+++ 3b-search-cols-to-grep.R
@@ -3,7 +3,7 @@
 ## Author: Austin Knuppe (knuppe.2@osu.edu)
 ## Project: Contracts Data
 ## Last Updated: 21 Oct. 2015
-## Specs: Mac OS 10.11, RStudio v. 0.99.486
+## Specs: Mac OS 10.11, RStudio v. 0.99.486, R v. 3.2.2
 ## =============================================================================
 
 ## Script Purpose: Grep through data table to find contract events related to
@@ -21,7 +21,7 @@ options(stringsAsFactors = FALSE)
 ##-------------------------------------------------
 # load a single time slice of the contracts dataset 
 load("~/Data/MDAP/r-data/clean-cols-to-grep.RData") 
-load("~/Data/MDAP/r-data/mdap-list.RData") 
+load("~/Data/MDAP/r-data/mdap_list.RData") 
 
 setkey(dat)
 
@@ -49,11 +49,25 @@ y2 <- dat$sec_descrip # SEC description
 y3 <- dat$contract_descrip # contract description
 y4 <- dat$major_program # major program code
 
+rm(dat)
+
+x1 <- c("162", "GAJ")
+x2 <- c("NAVSTAR Global Positioning System", "NAVSTAR")
+
 ##----------------------------------------------------- 
 # create a vector of searchable terms across covariates
-# THIS SHOULD BE A FUNCTION "set_terms"
-x1 <- c("220", "ACD") # mdap[[i], sec]
-x2 <- c("C-130", "Hercules") # mdap[[i], grep_terms]
+# sec <- list(mdap_list$sec)
+# grep_terms <- list(mdap_list$grep_terms)
+
+# grep_terms <- function(i){
+#       x1 <- print(sec[i])
+#       x2 <- print(grep_terms[i])
+# }
+
+## When I type grep_terms(), I want it to return x1 and x2 with unique values
+## from items in the list sec (e.g. x1 <- "123", and x2 <- "LCS")
+
+sys_time <- Sys.time()
 
 ## ------------------------------------
 # grep through system or equipment code
@@ -95,7 +109,11 @@ rm(obs_rows)
 
 ##------------------- 
 # save new data table
-save(mdap_obs, file = "~/Data/MDAP/r-data/mdap-c130.RData")
+save(mdap_obs, file = "~/Data/MDAP/r-data/Austin-MDAP/mdap-gps.RData")
+
+Sys.time() - sys_time
+
+rm(mdap_obs)
 
 #---------------------------------------
 # erase previous objects in system memory
@@ -104,3 +122,53 @@ rm(list=ls())
 ##=============================================================================
 ## END OF FILE
 ##=============================================================================
+
+
+
+
+
+
+set_grep_terms <- function(x){
+        x1 <- print(sec[x])
+        x2 <- print(grep_terms[x])
+}
+
+find_mdap <- function(x1, x2)
+        # Input: x1 and x2
+        # Output: a datatable with all obs containing values from x1 and x2
+{
+        grep_for_x_in_y <- function(x, y){
+                position_list <- lapply(x, function(x_element)
+                        grep(x_element, y, ignore.case = TRUE))
+                position_vector <- do.call(c, position_list)
+                sort(unique(position_vector))
+        }
+        
+        mdap_sec <- data.table(grep_for_x_in_y(x1, y1)) 
+        colnames(mdap_sec)[1] <- "rownumber" 
+        
+        mdap_sec_descrip <- data.table(grep_for_x_in_y(x2, y2)) 
+        colnames(mdap_sec_descrip)[1] <- "rownumber" 
+        
+        mdap_contract_descrip <- data.table(grep_for_x_in_y(x2, y3)) 
+        colnames(mdap_contract_descrip)[1] <- "rownumber" 
+        
+        mdap_program_code <- data.table(grep_for_x_in_y(x2, y4)) 
+        colnames(mdap_program_code)[1] <- "rownumber" 
+        
+        obs_rows <- unique(rbindlist(list(mdap_sec, mdap_sec_descrip, mdap_program_code,
+                                          mdap_contract_descrip)))
+        
+        rm(list = c("mdap_sec", "mdap_sec_descrip", "mdap_contract_descrip", 
+                    "mdap_program_code", "x1", "x2"))
+        
+        setkey(dat, rownumber)
+        
+        mdap_obs <- dat[obs_rows]
+        
+        rm(obs_rows)
+        
+        save("mdap_obs", paste0("~/Data/contracts/MDAP/r-data/mdap_", mdap_obs, ".RData"))
+        
+        rm(mdap_obs)
+}
